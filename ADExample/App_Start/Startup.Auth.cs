64b9dc3a2e1c5ed4e50b7be3382d2eae06d5df0e
adesharakeval <adesharakeval@gmail.com>
2024-04-21 15:29:52 +0530

    code commit

---
+++ ADExample/App_Start/Startup.Auth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace ADExample
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        private static string realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
+     
+     
+        private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
+
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    RedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error?message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ ADExample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ADExample.Startup))]
+
+
+
+
+namespace ADExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+      
+            ConfigureAuth(app);
+       
+        }
+    }
+}
